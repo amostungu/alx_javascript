a5eebde4a9d5ff859648e25591e866b91ba72fe3
amostungu <amostungu1@gmail.com>
2023-09-03 12:33:05 -0700

    Increment object

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,14 +2,11 @@
 const myObject = {
   type: 'object',
   value: 12
-  myObject.incr: function () {
-    this.value++;
-  }
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
+  myObject.incr: function () {
+    this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
