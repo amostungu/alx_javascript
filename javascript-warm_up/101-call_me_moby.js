ada678c6cdb3bd1a41cfc99cbceb2c143303828d
amostungu <amostungu1@gmail.com>
2023-09-03 12:03:17 -0700

    Call me Moby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-	let y = 0;
-	while(y < x) {
-		theFunction()
-		y++;
-	}
+  let y = 0;
+  while(y < x) {
+	  theFunction()
+	  y++;
+  }
 };
